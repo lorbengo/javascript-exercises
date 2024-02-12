fa09d712f2f7b817846857c151967779ddce5820
lorbengo <lorbengo@gmail.com>
2024-02-12 21:07:19 +0300

    Finish 07_tempConversion exercise

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,9 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(temp) {
+  return Number(Number.parseFloat((temp - 32.0) / 1.8).toFixed(1));
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(temp) {
+  return Number(Number.parseFloat((temp * 1.8) + 32).toFixed(1));
 };
 
 // Do not edit below this line
