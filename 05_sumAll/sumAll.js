4e1f9dd01ad93be21cac381d22e21fe5a27bd801
lorbengo <lorbengo@gmail.com>
2024-02-12 20:04:42 +0300

    Finish 05_sumAll exercise

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,18 @@
-const sumAll = function() {
-
+const sumAll = function(a, b) {
+  if (!Number.isInteger(a) || !Number.isInteger(b) || a < 0 || a < 0) {
+    return 'ERROR';
+  }
+  let sum = 0;
+  if (a < b) {
+    for (let i = a; i <= b; i++) {
+      sum += i;
+    }
+  } else {
+    for (let i = a; i >= b; i--) {
+      sum += i;
+    }
+  }
+  return sum;
 };
 
 // Do not edit below this line
