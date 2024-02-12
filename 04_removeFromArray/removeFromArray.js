eb8832741ac3674bd51751f97ce97d69be6a1cb6
lorbengo <lorbengo@gmail.com>
2024-02-12 19:18:36 +0300

    Change 04_removeFromArray exercise

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,9 +1,5 @@
 const removeFromArray = function(array, ...args) {
-  let result = array;
-  for (let arg of args) {
-    result = result.filter((elem) => elem !== arg);
-  }
-  return result;
+  return array.filter((item) => !args.includes(item));
 };
 
 // Do not edit below this line
