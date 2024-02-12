9eab4606159e292f26a538a8fd9c3b48572dd0bf
lorbengo <lorbengo@gmail.com>
2024-02-12 18:52:18 +0300

    Finish 04_removeFromArray exercise

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,9 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(array, ...args) {
+  let result = array;
+  for (let arg of args) {
+    result = result.filter((elem) => elem !== arg);
+  }
+  return result;
 };
 
 // Do not edit below this line
