04a55c3a1754d4e67319c7c0e99a807df1b3c1c9
lorbengo <lorbengo@gmail.com>
2024-02-21 12:31:04 +0300

    Finish 10_fibonacci exercise

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,16 @@
-const fibonacci = function() {
-
+const fibonacci = function (number) {
+  if (typeof number === "string") {
+    number = parseInt(number);
+  }
+  if (number < 0) {
+    return "OOPS";
+  }
+  let a = 1;
+  let b = 0;
+  for (let i = 0; i < number; i++) {
+    [a, b] = [b, b + a];
+  }
+  return b;
 };
 
 // Do not edit below this line
