9ba90be62771e5fe929feaf05aea2e6a5f3e40a1
lorbengo <lorbengo@gmail.com>
2024-02-21 16:16:31 +0300

    Finish 12_findTheOldest exercise

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,6 +1,20 @@
-const findTheOldest = function() {
-
+const findTheOldest = function (people) {
+  return people.reduce((oldestPerson, currentPerson) => {
+    const oldestAge = getAge(
+      oldestPerson.yearOfBirth,
+      oldestPerson.yearOfDeath
+    );
+    const currentAge = getAge(
+      currentPerson.yearOfBirth,
+      currentPerson.yearOfDeath
+    );
+    return oldestAge < currentAge ? currentPerson : oldestPerson;
+  });
 };
+
+function getAge(birth, death = new Date().getFullYear()) {
+  return death - birth;
+}
 
 // Do not edit below this line
 module.exports = findTheOldest;
